07d64263dfead0012b661e14a6bfee4566c39fa2
Dhanashree hakke <dhanashreehakke7@gmail.com>
2022-01-03 12:24:45 +0530

    Added: Multiple addressbook creation and stoaring data work in progress

--- src/main/java/com/example/addressbook/service/AddressBookService.java
+++ src/main/java/com/example/addressbook/service/AddressBookService.java
@@ -3,10 +3,12 @@ package com.example.addressbook.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.addressbook.dto.AddressBookDTO;
 import com.example.addressbook.model.AddressBookData;
 import com.example.addressbook.repository.IAddressBookRepository;
-
+@Service
 public class AddressBookService implements IAddressBookService {
 
 	@Autowired
@@ -15,6 +17,13 @@ public class AddressBookService implements IAddressBookService {
 	@Override
 	public List<AddressBookData> getAddressBookData() {
 		return addressBookRepositoty.findAll();
+	}
+
+	@Override
+	public AddressBookData createPersonData(AddressBookDTO addressDTO) {
+		AddressBookData addressData = null;
+		addressData  = new AddressBookData(addressDTO);
+		return addressBookRepositoty.save(addressData );
 	}
 
 }
--- src/main/java/com/example/addressbook/service/IAddressBookService.java
+++ src/main/java/com/example/addressbook/service/IAddressBookService.java
@@ -2,11 +2,13 @@ package com.example.addressbook.service;
 
 import java.util.List;
 
+import com.example.addressbook.dto.AddressBookDTO;
 import com.example.addressbook.model.AddressBookData;
-import com.example.addressbook.model.PersonData;
 
 public interface IAddressBookService {
 	
 	List<AddressBookData> getAddressBookData();
+
+	AddressBookData createPersonData(AddressBookDTO addressDTO);
 
 }
